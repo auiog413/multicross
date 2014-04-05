ab240f8d94e9c03a13cb82f85ed72ff50c36f775
auiog413 <lbzqh156@gmail.com>
2014-04-05 17:04:10 +0800

    添加zuitu系统

---
+++ zuitu/include/configure/db.php
@@ -0,0 +1,8 @@
+<?php
+$value = array (
+  'host' => '127.0.0.1',
+  'user' => 'root',
+  'pass' => '123456',
+  'name' => 'zuitu_db',
+);
+?>
---
+++ zuitu/include/configure/system.php
@@ -0,0 +1,15 @@
+<?php
+$value = array (
+  'wwwprefix' => 'http://zuitu.itron.me',
+  'imgprefix' => 'http://zuitu.itron.me',
+  'cssprefix' => 'http://zuitu.itron.me',
+  'sitename' => '最土网',
+  'sitetitle' => '精品团购每一天',
+  'abbreviation' => '最土',
+  'couponname' => '优惠券',
+  'currency' => '&#165;',
+  'currencyname' => 'CNY',
+  'timezone' => 'Etc/GMT-8',
+  'editor' => 'kind',
+);
+?>
